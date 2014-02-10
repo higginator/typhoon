bfd21c9cde8678bb38a29fdbfe6088d762782e74
Ryan Higgins <ryanhigs@gmail.com>
2014-02-10 00:00:00 -0800

    req file updated

--- typhoon/settings.py
+++ typhoon/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'typhoon.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'ryan',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
